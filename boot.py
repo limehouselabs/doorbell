f321d811db7fb5d3842bfe080482ae0cf7f67567
Lex Robinson <lex@lex.me.uk>
2019-06-24 21:55:41 +0100

    Delay startup until la wifi has connected

--- boot.py
+++ boot.py
@@ -10,11 +10,30 @@ import network
 
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
-if sta_if.active():
-    ifconfig = sta_if.ifconfig()
-    print("Connected! IP:", ifconfig[0])
-else:
-    print("Failed to connect :(")
+if not sta_if.isconnected():
+    while True:
+        status = sta_if.status()
+        if status == network.STAT_IDLE:
+            print("ERROR: Not doing anything??")
+            machine.deepsleep()
+        elif status == network.STAT_GOT_IP:
+            print("Connected!")
+            break
+        elif status == network.STAT_WRONG_PASSWORD:
+            print("ERROR: AP Password has changed!")
+            machine.deepsleep()
+        elif status == network.STAT_NO_AP_FOUND:
+            print("WARNING: No AP found...? Sleeping for 1 minute.")
+            machine.deepsleep(60000)
+        elif status == network.STAT_CONNECT_FAIL:
+            print("ERROR: Unable to connect :(")
+            machine.deepsleep(300000)
+        else:
+            print("Connecting...")
+            machine.lightsleep(1000)
+
+ifconfig = sta_if.ifconfig()
+print("IP:", ifconfig[0])
 
 webrepl.start()
 
